8cc9997725ec10e3c600022fe83f548cc35222e8
FrumptiosCrown <anas200598@gmail.com>
2026-05-31 00:31:38 +0200

    Projektdateien hinzufügen.

---
+++ TextAdventureGame/Sources/Models/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAdventureGame.Sources.Models
+{
+    class Stats
+    {
+        public double strength = 0;
+        public double dexterity = 0;
+        public double constitution = 0; 
+        public double intelligence = 0;
+        public Stats() 
+        { }
+    }
+}
